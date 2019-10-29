e578bb394d932e92062a812c21d9065df496bd59
@trevorgriest <tgriest1981@gmail.com>
2019-10-29 13:55:21 -0400

    controller update

--- backend/AlbumCollection/AlbumCollection/Controllers/ArtistController.cs
+++ backend/AlbumCollection/AlbumCollection/Controllers/ArtistController.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AlbumCollection.Controllers
 {
-    public class ArtistController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Artist : ControllerBase
     {
+        // GET api/Artist
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "artist1", "artist2" };
+        }
+
+        // GET api/Artist/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "Artist";
+        }
+
+        // POST api/artist
+        [HttpPost]
+        public void Post([FromBody] string Artist)
+        {
+        }
+
+        // PUT api/artist/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string Artist)
+        {
+        }
+
+        // DELETE api/artist/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
 }
