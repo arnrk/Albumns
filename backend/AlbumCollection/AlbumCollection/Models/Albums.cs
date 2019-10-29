3cb5b84013d14b68f53895b55ac25b47a5e06087
Aaron Rock <aaronrk@mail.com>
2019-10-29 13:55:13 -0400

    models

--- backend/AlbumCollection/AlbumCollection/Models/Albums.cs
+++ backend/AlbumCollection/AlbumCollection/Models/Albums.cs
@@ -7,5 +7,22 @@ namespace AlbumCollection.Models
 {
     public class Albums
     {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Recordlabel { get; set; }
+    
+
+        public Albums()
+        {
+        }
+
+        public Albums(int id, string title, string recordLabel)
+        {
+            ID = id;
+            Title = title;
+            Recordlabel = recordLabel;
+
+        }
+
     }
 }
--- backend/AlbumCollection/AlbumCollection/Models/Artists.cs
+++ backend/AlbumCollection/AlbumCollection/Models/Artists.cs
@@ -7,5 +7,28 @@ namespace AlbumCollection.Models
 {
     public class Artists
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Recordlabel { get; set; }
+        public string Hometown { get; set; }
+   
+
+        public Artists()
+        {
+        }
+
+
+        public Artists(int id, string name, string age, string recordLabel, string hometown)
+        {
+        ID = id;
+        Name = name;
+        Age = age;
+        Recordlabel = recordLabel;
+        Hometown = hometown;
+        }
+
     }
+
+
 }
