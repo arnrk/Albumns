370f67a0f14ed0db37a2700e1f16882a20a3fadd
@trevorgriest <tgriest1981@gmail.com>
2019-10-30 15:26:38 -0400

    added components/js

---
+++ frontend/src/js/components/Header.js
@@ -0,0 +1,13 @@
+export default function Header(){
+    return `
+    <nav class='nav__header'>
+    <ul>
+    <li class='nav__home'>Home</li>
+    <li class='values'>Values</li>
+    <li class='todos'>ToDos</li>
+    
+    </ul>
+
+</nav>
+    `;
+}
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,3 +1,23 @@
+import Header from "./components/header";
+import Footer from "./components/footer";
+
 export default () => {
-    alert("it works")
-  }
+    pageBuild();
+    
+};
+
+function pageBuild(){
+    header();
+    footer();
+}
+
+function header(){
+    const header = document.getElementById("header");
+    header.innerHTML = Header();
+    
+}
+
+function footer(){
+    const footer = document.getElementById("footer");
+    footer.innerHTML = Footer();
+}
