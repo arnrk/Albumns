68de1c6a837cfe6eb8b5056f3237c5db4ca6be0f
Steven <steventruong1995@yahoo.com>
2019-10-31 10:12:43 -0400

    added component folders

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,14 +1,19 @@
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Album from "./components/Album";
+import Artist from "./components/Artist";
+import Song from "./components/Song";
 
 export default () => {
     pageBuild();
-    
 };
 
 function pageBuild(){
     header();
     footer();
+    album();
+    artist();
+    song();
 }
 
 function header(){
@@ -20,4 +25,19 @@ function header(){
 function footer(){
     const footer = document.getElementById("footer");
     footer.innerHTML = Footer();
+}
+
+function album(){
+    const album = document.getElementById("album")
+    album.innerHTML = Album();
+}
+
+function artist(){
+    const album = document.getElementById("artist")
+    album.innerHTML = Artist();
+}
+
+function song(){
+    const album = document.getElementById("song")
+    album.innerHTML = Song();
 }
