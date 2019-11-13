741ac6865c55963025f70bb3dbeb766e79d838be
Steven <steventruong1995@yahoo.com>
2019-11-12 19:28:59 -0500

    gave songs background image

--- frontend/src/js/components/Song.js
+++ frontend/src/js/components/Song.js
@@ -4,6 +4,7 @@ export default function Song(songs) {
     ${songs
       .map(song => {
         return `
+        <div id="songbox">
             <li>
                 <h3>${song.name}</h3>
                 <h4>${song.duration}</h4>
@@ -11,6 +12,7 @@ export default function Song(songs) {
                       <button class="edit-song__submit" id="buttonCrud">Edit</button>
                       <button class="delete-song__submit" id="buttonCrud">Delete</button>
             </li>
+          </div>
         `;
       })
       .join("")}
